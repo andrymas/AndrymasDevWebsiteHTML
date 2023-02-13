cc7304dfb9c520ff53a46eaf0801970825376476
andrymas <46085583+andrymas@users.noreply.github.com>
2023-02-14 00:25:54 +0100

    lu

--- carissima_mia/code.js
+++ carissima_mia/code.js
@@ -1,10 +1,16 @@
 imgArray = [
-    "1.png",
-    "2.png",
-    "3.png",
-    "4.png",
-    "5.png",
-    "6.png"
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
+    "11.jpg",
+    "12.jpg",
     ]
     
     
