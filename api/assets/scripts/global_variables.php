63af802e30b44fc667bcbb21777f56ef736b73fe
Andrymas <andreamas07@gmail.com>
2024-03-28 16:52:09 +0100

    fix

--- api/assets/scripts/global_variables.php
+++ api/assets/scripts/global_variables.php
@@ -1,7 +1,7 @@
 <?php
     $bg_string = "#131516";
-    $home_add = $_SERVER['DOCUMENT_ROOT']."/api/index.php";
-    $proj_add = $_SERVER['DOCUMENT_ROOT']."/api/projects.php";
+    $home_add = "/api/index.php";
+    $proj_add = "/api/projects.php";
 
     $tf_percentage = 75;
     $tf_last_update = "17 June 2022";
