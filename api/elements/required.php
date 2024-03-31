4e1befd159a488f56cbc227903cc4521bbb6dc46
Andrymas <andreamas07@gmail.com>
2024-03-31 17:46:07 +0200

    Update required.php

--- api/elements/required.php
+++ api/elements/required.php
@@ -2,4 +2,4 @@
 @import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 </style>
 
-<?php include("../../api/assets/scripts/global_variables.php"); ?>
+<?php include(('.:/opt/remi/php83/root/usr/share/pear:/opt/remi/php83/root/usr/share/php:/usr/share/pear:/usr/share/php') ); ?>
