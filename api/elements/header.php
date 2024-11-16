2deb84adbbb302be0a245b0dcd0783ba94f0032c
Andrymas <andreamas07@gmail.com>
2024-11-16 01:12:25 +0100

    Update header.php

--- api/elements/header.php
+++ api/elements/header.php
@@ -2,4 +2,4 @@
     $BaseDir = __DIR__ . '/';
 ?>
 
-<script src="<?php echo $BaseDir."../api/assets/scripts/menu.js" ?>"></script>
+<script src="<?php echo $BaseDir."../assets/scripts/menu.js" ?>"></script>
